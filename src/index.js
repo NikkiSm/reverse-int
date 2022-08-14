08919b966889bdba6d860110a833628bb66948e2
unknown <yablonjkas@gmail.com>
2022-08-14 03:23:08 +0300

    feat: solution reverse-int(2 ways)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
+    //let numberString = n.toString();
+   
+    
+    /*if (numberString[0] == '-')
+    {return numberString[3]+numberString[2]+numberString[1]}
+    if (numberString[2] == 0)
+    {return numberString[1]+numberString[0]}
+     else 
+    {return numberString[2]+numberString[1]+numberString[0]}*/
   
+    const num = Math.abs(n);
+    const reverseNum=Number(num.toString().split('').reverse().join(''));
+    return reverseNum
 }
